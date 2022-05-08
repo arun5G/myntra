627e38b7d8ddd47fb7cbd279ec170cb1caa20bc5
Arijit Bhatta <arijitbhatta123@gmail.com>
2022-05-08 11:43:43 +0530

    login&signup

---
+++ SIGNUPLOGIN/signup.js
@@ -0,0 +1,23 @@
+var deatils = JSON.parse(localStorage.getItem("Details")) || []
+document.querySelector("button").addEventListener("click",newA)
+function newA(){
+    deatils.userPassword=document.querySelector("#password").value
+    deatils.userName=document.querySelector("#name").value
+    deatils.userEmail=document.querySelector("#email").value
+    deatils.userGender=document.querySelector("#gender").value
+    deatils.userAlternateMobile=document.querySelector("#ALnum").value
+    deatils.userHint=document.querySelector("#hint").value
+    
+    console.log(deatils)
+
+    if(deatils.userPassword==""  || deatils.userName=="" || deatils.userEmail=="" || deatils.userGender=="")
+    {
+        alert("Fill The Empty Boxes")
+    }
+    else
+    {
+        localStorage.setItem("Details", JSON.stringify(deatils))
+        window.location.href="./../folder home page/home.html"
+    }
+}
+
---
+++ SIGNUPLOGIN/signupOrLogin.js
@@ -0,0 +1,6 @@
+document.querySelector("button").addEventListener("submit",newAccount)
+function newAccount(){
+    // event.preventDefault
+    window.location.href="signup.html"
+    console.log("working")
+}
